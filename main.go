5d5d7e59599b96071846a9c57bf06f883bbafaa9
tay3223 <tay3223@qq.com>
2021-12-09 12:18:59 +0800

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "GoMessage/routers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/"] = "swagger"
+	}
+	beego.Run()
+}
---
+++ routers/router.go
@@ -0,0 +1,30 @@
+// @APIVersion 1.0.0
+// @Title 信息转发器
+// @Description 转发Prometheus生态圈中AlertManager推送的webhook信息到钉钉或微信等
+// @Contact tay3223@11.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"GoMessage/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+		beego.NSNamespace("/object",
+			beego.NSInclude(
+				&controllers.ObjectController{},
+			),
+		),
+		beego.NSNamespace("/user",
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
