4bdd2ed14634b900e90c7c76b65604f62c5be150
tay3223 <tay3223@qq.com>
2023-02-25 15:37:48 +0800

    update

--- apps/views/base.go
+++ apps/views/base.go
@@ -1,27 +1,27 @@
 package views
 
 type ResponseTemplate struct {
-	Code   int    `json:"code"`   //此处约定：1代表成功，0代表失败
-	Msg    string `json:"msg"`    //对请求结果的描述消息，可以为空
-	Result any    `json:"result"` //请求成功后给出的结果值
-	Error  error  `json:"error"`  //如果请求失败，这里一定要给出错误信息
-	Help   string `json:"help"`
+    Code   int    `json:"code"`   //此处约定：1代表成功，0代表失败
+    Msg    string `json:"msg"`    //对请求结果的描述消息，可以为空
+    Result any    `json:"result"` //请求成功后给出的结果值
+    Error  error  `json:"error"`  //如果请求失败，这里一定要给出错误信息
+    Help   string `json:"help"`
 }
 
 func ResponseSuccessful(msg string, result any) ResponseTemplate {
-	return ResponseTemplate{
-		Code:   1,
-		Help:   "帮助信息请查看( https://github.com/gomessage/gomessage/blob/master/wiki/response.md )",
-		Msg:    msg,
-		Result: result,
-	}
+    return ResponseTemplate{
+        Code:   1,
+        Msg:    msg,
+        Help:   "帮助信息请查看：https://github.com/gomessage/gomessage/blob/master/wiki/response.md ",
+        Result: result,
+    }
 }
 
 func ResponseFailure(msg string, error error) ResponseTemplate {
-	return ResponseTemplate{
-		Code:  0,
-		Help:  "帮助信息请查看( https://github.com/gomessage/gomessage/blob/master/wiki/response.md )",
-		Msg:   msg,
-		Error: error,
-	}
+    return ResponseTemplate{
+        Code:  0,
+        Msg:   msg,
+        Help:  "帮助信息请查看：https://github.com/gomessage/gomessage/blob/master/wiki/response.md ",
+        Error: error,
+    }
 }
--- apps/views/viewNamespace.go
+++ apps/views/viewNamespace.go
@@ -1,12 +1,12 @@
 package views
 
 import (
-	"errors"
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"gomessage/apps/models"
-	"net/http"
-	"strconv"
+    "errors"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "gomessage/apps/models"
+    "net/http"
+    "strconv"
 )
 
 // ListNamespace
@@ -14,18 +14,18 @@ import (
 // @Summary 获取所有命名空间
 // @Router /api/v1/namespace [GET]
 func ListNamespace(g *gin.Context) {
-	isActive := g.DefaultQuery("is_active", "")
-	switch isActive {
-	case "true", "false", "1", "0", "":
-		list, err := models.ListNamespace(isActive)
-		if err != nil {
-			g.JSON(http.StatusInternalServerError, ResponseFailure("服务器内部错误", err))
-		}
-		g.JSON(http.StatusOK, ResponseSuccessful("查询成功", list))
+    isActive := g.DefaultQuery("is_active", "")
+    switch isActive {
+    case "true", "false", "1", "0", "":
+        list, err := models.ListNamespace(isActive)
+        if err != nil {
+            g.JSON(http.StatusInternalServerError, ResponseFailure("服务器内部错误", err))
+        }
+        g.JSON(http.StatusOK, ResponseSuccessful("查询成功", list))
 
-	default:
-		g.JSON(http.StatusBadRequest, ResponseFailure("参数错误", errors.New("is_active的值只能为布尔值true、false")))
-	}
+    default:
+        g.JSON(http.StatusBadRequest, ResponseFailure("参数错误", errors.New("is_active的值只能为布尔值true、false")))
+    }
 }
 
 // PostNamespace
@@ -33,16 +33,16 @@ func ListNamespace(g *gin.Context) {
 // @Summary 新增一个命名空间
 // @Router /api/v1/namespace [POST]
 func PostNamespace(g *gin.Context) {
-	body := models.Namespace{}
-	if err := g.ShouldBindJSON(&body); err != nil {
-		return
-	}
-	namespace, err := models.AddNamespace(&body)
-	if err != nil {
-		g.JSON(http.StatusBadRequest, ResponseFailure("命名空间已存在，不能重复创建", err))
-	} else {
-		g.JSON(http.StatusOK, ResponseSuccessful("命名空间创建成功", &namespace))
-	}
+    body := models.Namespace{}
+    if err := g.ShouldBindJSON(&body); err != nil {
+        return
+    }
+    namespace, err := models.AddNamespace(&body)
+    if err != nil {
+        g.JSON(http.StatusBadRequest, ResponseFailure("命名空间已存在，不能重复创建", err))
+    } else {
+        g.JSON(http.StatusOK, ResponseSuccessful("命名空间创建成功", &namespace))
+    }
 }
 
 // GetNamespace
@@ -50,14 +50,14 @@ func PostNamespace(g *gin.Context) {
 // @Summary 查询一个命名空间
 // @Router /api/v1/namespace/:id [GET]
 func GetNamespace(g *gin.Context) {
-	//id, _ := strconv.ParseInt(g.Param("id"), 10, 64)
-	id, _ := strconv.Atoi(g.Param("id"))
-	result, err := models.GetNamespaceById(id)
-	if err != nil {
-		g.JSON(http.StatusBadRequest, ResponseFailure("参数错误", err))
-	} else {
-		g.JSON(http.StatusOK, ResponseSuccessful("查询成功", result))
-	}
+    //id, _ := strconv.ParseInt(g.Param("id"), 10, 64)
+    id, _ := strconv.Atoi(g.Param("id"))
+    result, err := models.GetNamespaceById(id)
+    if err != nil {
+        g.JSON(http.StatusBadRequest, ResponseFailure("参数错误", err))
+    } else {
+        g.JSON(http.StatusOK, ResponseSuccessful("查询成功", result))
+    }
 }
 
 // PutNamespace
@@ -65,16 +65,16 @@ func GetNamespace(g *gin.Context) {
 // @Summary 修改一个命名空间
 // @Router /api/v1/namespace/:id [PUT]
 func PutNamespace(g *gin.Context) {
-	id, _ := strconv.Atoi(g.Param("id"))
-	body := models.Namespace{}
-	g.ShouldBindJSON(&body)
+    id, _ := strconv.Atoi(g.Param("id"))
+    body := models.Namespace{}
+    g.ShouldBindJSON(&body)
 
-	result, err := models.UpdateNamespace(id, &body)
-	if err != nil {
-		g.JSON(http.StatusBadRequest, "Namespace名称不能重复")
-	} else {
-		g.JSON(http.StatusOK, result)
-	}
+    result, err := models.UpdateNamespace(id, &body)
+    if err != nil {
+        g.JSON(http.StatusBadRequest, ResponseFailure("namespace名称不能重复", err))
+    } else {
+        g.JSON(http.StatusOK, ResponseSuccessful("修改成功", result))
+    }
 }
 
 // DeleteNamespace
@@ -82,11 +82,11 @@ func PutNamespace(g *gin.Context) {
 // @Summary 删除一个命名空间
 // @Router /api/v1/namespace/:id [DELETE]
 func DeleteNamespace(g *gin.Context) {
-	id, _ := strconv.Atoi(g.Param("id"))
-	num, err := models.DeleteNamespace(id)
-	if err != nil {
-		g.JSON(http.StatusBadRequest, err)
-	} else {
-		g.JSON(http.StatusOK, fmt.Sprintf("受影响的行数：%v", num))
-	}
+    id, _ := strconv.Atoi(g.Param("id"))
+    num, err := models.DeleteNamespace(id)
+    if err != nil {
+        g.JSON(http.StatusBadRequest, ResponseFailure("删除失败", err))
+    } else {
+        g.JSON(http.StatusOK, ResponseSuccessful("删除操作执行成功", fmt.Sprintf("受影响的行数：%v", num)))
+    }
 }
