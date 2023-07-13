e99770527500bf0040aa18f07f987ddb2f30031a
古寒飞 <tay3223@qq.com>
2023-07-13 22:17:01 +0800

    update

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"gomessage/pkg/routers"
-	initialize2 "gomessage/pkg/utils/initialize"
-	loggers2 "gomessage/pkg/utils/log/loggers"
+	"gomessage/pkg/utils/initialize"
+	"gomessage/pkg/utils/log/loggers"
 )
 
 func init() {
@@ -23,14 +23,14 @@ func init() {
 	 *
 	 * **********************************************
 	 */
-	initialize2.InitConfig()  //应该被第一个执行
-	initialize2.InitCmd()     //初始化环境变量，应该紧跟在InitConfig后面，被第二个执行，cmd模块接受各种启动参数，通常用来覆盖config中的一些内容
-	loggers2.InitRuntimeLog() //初始化runtime日志模块，应该紧跟在InitEnv后面，被第三个执行（剩下的其它模块初始化，启动顺序就没有什么要求了）
-	loggers2.InitPushLogger()
-	loggers2.InitAccessLog()
-	initialize2.InitSwagger()
-	initialize2.InitGinMode()
-	initialize2.InitDB("sqlite3", initialize2.GlobalVars.Migrate)
+	initialize.InitConfig()  //应该被第一个执行
+	initialize.InitCmd()     //初始化环境变量，应该紧跟在InitConfig后面，被第二个执行，cmd模块接受各种启动参数，通常用来覆盖config中的一些内容
+	loggers.InitRuntimeLog() //初始化runtime日志模块，应该紧跟在InitEnv后面，被第三个执行（剩下的其它模块初始化，启动顺序就没有什么要求了）
+	loggers.InitPushLogger()
+	loggers.InitAccessLog()
+	initialize.InitSwagger()
+	initialize.InitGinMode()
+	initialize.InitDB("sqlite3", initialize.GlobalVars.Migrate)
 }
 
 func main() {
@@ -54,7 +54,7 @@ func main() {
 	host := viper.GetString("service.host")
 	port := viper.GetString("service.port")
 	serviceAddress := fmt.Sprintf("%s:%s", host, port)
-	loggers2.DefaultLogger.Infof("服务监听：http://%s", serviceAddress)
+	loggers.DefaultLogger.Infof("服务监听：http://%s", serviceAddress)
 	if err := r.Run(serviceAddress); err != nil {
 		return
 	}
