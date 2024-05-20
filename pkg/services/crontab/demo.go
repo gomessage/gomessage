425c135080e3b446d6ea24b318d16f9be2b6bf3b
古寒飞 <tay3223@qq.com>
2024-05-20 13:06:49 +0800

    v2.3.17--定时消息功能开发进行中

--- pkg/services/crontab/demo.go
+++ pkg/services/crontab/demo.go
@@ -6,13 +6,25 @@ import (
 	"time"
 )
 
-func main() {
-	c := cron.New()
-	c.AddFunc("@every 1s", func() {
-		fmt.Println("每秒打印一次")
-	})
+func test1() {
+	fmt.Println("hello world打印一次")
+}
 
+func main() {
+	crontabRule := "@every 1s"
+
+	c := cron.New()
 	c.Start()
+	defer c.Stop()
+	fmt.Println("cron启动")
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("暂停5秒钟")
+
+	c.AddFunc(crontabRule, test1)
+	fmt.Println("添加定时任务")
+
 	time.Sleep(time.Second * 20)
+	fmt.Println("暂停20秒")
 
 }
